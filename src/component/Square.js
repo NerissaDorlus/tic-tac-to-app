dc741df0a1c9d45f2bc2dda3e4a7650ba63d36c6
Nerissa Dorlus <nerissadorlus@tdas-pro-3.lan>
2022-05-29 12:49:59 -0400

    added useState and handleClick

--- src/component/Square.js
+++ src/component/Square.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import "../index.css";
 
-const Square = ({ value }) => {
-  return <button className="square">{value}</button>;
+const Square = () => {
+  const [value, setValue] = useState("");
+
+  const handleClick = () => {
+    setValue("X");
+  };
+
+  return (
+    <button className="square" onClick={() => handleClick()}>
+      {value}
+    </button>
+  );
 };
 
 export default Square;
