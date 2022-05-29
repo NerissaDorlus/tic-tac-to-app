f435080f9d91bda3c0a0cee0fe09ba2d94494ed5
Nerissa Dorlus <nerissadorlus@tdas-pro-3.lan>
2022-05-29 11:22:51 -0400

    removed comments

--- src/index.js
+++ src/index.js
@@ -2,12 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-// class Square extends React.Component {
-//   render() {
-//     return <button className="square">{/* TODO */}</button>;
-//   }
-// }
-
 const Square = () => {
   return <button className="square">{/* TODO */}</button>;
 };
